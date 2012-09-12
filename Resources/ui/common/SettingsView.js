c99077cd15b71b28aeff1bfceb01ce6fe516be37
Timothy Andrew <mail@timothyandrew.net>
2012-09-12 16:46:52 +0530

    Server URL can now be entered in SettingsView.

--- Resources/ui/common/SettingsView.js
+++ Resources/ui/common/SettingsView.js
@@ -1,17 +1,27 @@
 //SettingsView Component Constructor
 function SettingsView() {
-	self = Ti.UI.createView();
-	
+	self = Ti.UI.createView({layout: 'vertical'});
+
 	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
 	var label = Ti.UI.createLabel({
-		color:'#000000',
-		text: 'This is the settings pane!',
-		height:'auto',
-		width:'auto'
+		color : '#000000',
+		text : 'Server location',
+		height : 'auto',
+		width : 'auto',
+		left: 5
 	});
 	self.add(label);
-	
+
+	var textField = Ti.UI.createTextField({
+		borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+		color : '#336699',
+		right: 5,
+		left: 5,
+		value: 'http://survey-web-staging.herokuapp.com/'		
+	});
+	self.add(textField);
+
 	return self;
 }
 
-module.exports = SettingsView;
+module.exports = SettingsView; 
