8a2f4ac4b05711fcd7bc39f749b7afe3278bd273
Timothy Andrew <mail@timothyandrew.net>
2012-09-13 14:53:59 +0530

    Table pulls data from the (local) DB on first run.

--- Resources/ui/common/SurveysIndexView.js
+++ Resources/ui/common/SurveysIndexView.js
@@ -1,10 +1,15 @@
 //SurveysIndexView Component Constructor
 function SurveysIndexView(model) {
+	var convertModelDataForTable = function() {
+		var _ = require('lib/underscore')._;
+		return _(model.list()).map(function(name) {
+			return { title : name }
+		});
+	}
+
 	Ti.App.addEventListener('surveys.fetch.success', function(e) {
 		var _ = require('lib/underscore')._;
-		var data = _(model.list()).map(function(name){
-			return { title: name }
-		});
+		data = convertModelDataForTable();
 		table.setData(data);
 	});
 
@@ -13,7 +18,7 @@ function SurveysIndexView(model) {
 
 	// now assign that array to the table's data property to add those objects as rows
 	var table = Titanium.UI.createTableView({
-	
+		data : convertModelDataForTable()
 	});
 
 	self.add(table);
