518ef874d20363cb767ada5a5898af7a1deb7ff0
Timothy Andrew <mail@timothyandrew.net>
2012-09-13 14:44:50 +0530

    Save surveys in the SQLite DB.

--- Resources/models/survey.js
+++ Resources/models/survey.js
@@ -1,6 +1,8 @@
 var Survey = function() {
-	this.surveys = [];
-	
+	this._ = require('lib/underscore')._;
+	this.open_db();
+	this.db.execute('CREATE TABLE IF NOT EXISTS surveys(id INTEGER PRIMARY KEY, name TEXT);');
+	this.close_db();
 }
 
 Survey.prototype = {
@@ -11,7 +13,15 @@ Survey.prototype = {
 			// function called when the response data is available
 			onload : function(e) {
 				Ti.API.info("Received text: " + this.responseText);
-				that.surveys = JSON.parse(this.responseText);
+				data = JSON.parse(this.responseText); // Emptying the table for now (until we get all the survey info from the server)
+				
+				that.open_db();
+				that.db.execute('DELETE FROM surveys;');				
+				that._(data).each(function(survey_name) {
+					that.db.execute('INSERT INTO surveys (name) VALUES (?)', survey_name);
+				});
+				that.close_db();
+				
 				Ti.App.fireEvent('surveys.fetch.success');
 			},
 			// function called when an error occurs, including a timeout
@@ -28,10 +38,28 @@ Survey.prototype = {
 	},
 
 	list : function() {
-		return this.surveys;
+		var surveys = [];
+		this.open_db();
+		var survey = this.db.execute('SELECT id,name FROM surveys');
+		while (survey.isValidRow()) {
+			var survey_name = survey.fieldByName('name');
+			//Ti.API.info(survey_name);
+			surveys.push(survey_name);
+			survey.next();
+		}
+		survey.close();
+		this.close_db();
+		return surveys;
+	},
+	
+	open_db : function() {
+		this.db = Ti.Database.open('SurveyMobile');
+	},
+	
+	close_db : function() {
+		this.db.close();
 	}
 };
-
 
 module.exports = Survey;
 
