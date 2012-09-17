47c3e9887a50f1d26ba6ea319089f64135035baa
Timothy Andrew <mail@timothyandrew.net>
2012-09-17 14:59:39 +0530

    ⌘-Shift-F on ApplicationWindow.js.

--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -5,44 +5,48 @@ function ApplicationWindow() {
 	var SettingsView = require('ui/common/SettingsView')
 	var SurveyShowView = require('ui/common/SurveyShowView')
 	var Survey = require('models/survey');
-	
+
 	var surveyModel = new Survey();
-	
-	var mainAppWindow = Ti.UI.createWindow({ title: 'Surveys' });
+
+	var mainAppWindow = Ti.UI.createWindow({
+		title : 'Surveys'
+	});
 	var surveysIndexView = new SurveysIndexView(surveyModel);
 	mainAppWindow.add(surveysIndexView);
 
-	var settingsWindow = Ti.UI.createWindow({ title: 'Settings' });
+	var settingsWindow = Ti.UI.createWindow({
+		title : 'Settings'
+	});
 	var settingsView = new SettingsView();
 	settingsWindow.add(settingsView);
-	
-	var surveyShowWindow = Ti.UI.createWindow({ title: 'Survey Details' });
-	var surveyShowView = new SurveyShowView();
-	surveyShowWindow.add(surveyShowView);
-		
+
 	//create component instance
 	var self = Ti.UI.createWindow({
-		backgroundColor:'#fff'
+		backgroundColor : '#fff'
 	});
 
-	var refreshButton = Ti.UI.createButton({ systemButton: Ti.UI.iPhone.SystemButton.REFRESH });
-	refreshButton.addEventListener('click', function(e){
+	var refreshButton = Ti.UI.createButton({
+		systemButton : Ti.UI.iPhone.SystemButton.REFRESH
+	});
+	refreshButton.addEventListener('click', function(e) {
 		surveyModel.fetch();
 	});
 	mainAppWindow.rightNavButton = refreshButton;
 
-	var settingsButton = Ti.UI.createButton({ title: 'Settings' });		
-	settingsButton.addEventListener('click', function(e){
+	var settingsButton = Ti.UI.createButton({
+		title : 'Settings'
+	});
+	settingsButton.addEventListener('click', function(e) {
 		navGroup.open(settingsWindow);
 	});
 	mainAppWindow.leftNavButton = settingsButton;
-	
+
 	var navGroup = Ti.UI.iPhone.createNavigationGroup({
-		window: mainAppWindow
+		window : mainAppWindow
 	});
 	self.add(navGroup);
-	
-	Ti.App.addEventListener('settings_saved', function(){
+
+	Ti.App.addEventListener('settings_saved', function() {
 		navGroup.close(settingsWindow);
 	})
 
@@ -53,7 +57,7 @@ function ApplicationWindow() {
 		surveyShowWindow.add(new SurveyShowView(surveyModel, e.surveyID));
 		navGroup.open(surveyShowWindow);
 	});
-	
+
 	return self;
 }
 
