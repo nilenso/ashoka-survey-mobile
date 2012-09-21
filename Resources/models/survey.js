df64d5ebdf1a064d4c51180ff78e86dcffee5651
Srihari Sriraman <srihari@c42.in>
2012-09-21 18:12:35 +0530

    Added a function to fetch all questions for a survey

--- Resources/models/survey.js
+++ Resources/models/survey.js
@@ -52,6 +52,27 @@ var Survey = new Ti.App.joli.model({
 
 		isEmpty : function() {
 			return this.count() == 0;
+		},
+		
+		fetchQuestions : function(surveyID) {
+			var url = Ti.App.Properties.getString('server_url') + '/api/mobile/surveys/' + surveyID ;
+			Ti.API.info("foooooo" + url)
+			var client = Ti.Network.createHTTPClient({
+				// function called when the response data is available
+				onload : function(e) {
+					Ti.API.info("Received text: " + this.responseText);
+					data = JSON.parse(this.responseText);
+				},
+				// function called when an error occurs, including a timeout
+				onerror : function(e) {
+					Ti.API.info("Foooooo Error");
+				},
+				timeout : 5000 // in milliseconds
+			});
+			// Prepare the connection.
+			client.open("GET", url);
+			// Send the request.
+			client.send();	
 		}
 	}
 });
