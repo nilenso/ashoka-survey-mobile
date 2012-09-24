66b10d6d1b5fad94c08626a6e0bf353af976b297
Timothy Andrew <mail@timothyandrew.net>
2012-09-24 17:40:39 +0530

    Added answer textfield to each question for a survey

--- Resources/ui/common/questions/QuestionsShowView.js
+++ Resources/ui/common/questions/QuestionsShowView.js
@@ -3,24 +3,38 @@ function QuestionsShowView(surveyID) {
 	var _ = require('lib/underscore')._;
 	var Question = require('models/question');
 
-	var convertQuestionDataForTable = function() {
-		var data = [];
-		var questions = Question.findBy('survey_id', surveyID);
-		_(questions).each(function(question) {
-			data.push({
-				title : question['content']
-			});
-			Ti.API.info("foo" + data);
+	var table = Titanium.UI.createTableView();
+	var tableData = []
+
+	var questions = Question.findBy('survey_id', surveyID);
+	_(questions).each(function(question) {
+		var label_row = Titanium.UI.createTableViewRow();
+		var label = Ti.UI.createLabel({
+			color : '#000000',
+			text : question['content'],
+			height : 'auto',
+			width : 'auto',
+			left : 5
 		});
-		return data;
-	}
-	self = Ti.UI.createView({
-		layout : 'vertical'
+		label_row.add(label);
+		tableData.push(label_row);
+
+		var answer_row = Titanium.UI.createTableViewRow();
+		var textField = Ti.UI.createTextField({
+			borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+			color : '#336699',
+			right : 5,
+			left : 5,
+			value : 'Answer'
+		});
+		answer_row.add(textField);
+		tableData.push(answer_row);
 	});
 
-	// now assign that array to the table's data property to add those objects as rows
-	var table = Titanium.UI.createTableView({
-		data : convertQuestionDataForTable(),
+	table.setData(tableData);
+
+	self = Ti.UI.createView({
+		layout : 'vertical'
 	});
 
 	self.add(table);
