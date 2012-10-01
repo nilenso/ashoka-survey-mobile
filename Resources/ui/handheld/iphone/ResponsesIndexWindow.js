f5b2cfdf79374d3b0a78e71f24d74c8d6bf56596
Timothy Andrew <mail@timothyandrew.net>
2012-10-01 12:16:13 +0530

    Got responses sync working on iPhone
    Needed to add some window/event stuff.

--- Resources/ui/handheld/iphone/ResponsesIndexWindow.js
+++ Resources/ui/handheld/iphone/ResponsesIndexWindow.js
@@ -4,30 +4,46 @@ function ResponsesIndexWindow(surveyID) {
 	var ResponseShowView = require('ui/common/responses/ResponseShowView')
 	var ResponseShowWindow = require('ui/handheld/iphone/ResponseShowWindow')
 	var Survey = require('models/survey');
-	
+
 	var self = Ti.UI.createWindow({
 		title : 'All Responses',
 		backgroundColor : "#fff"
 	});
 	self.add(new ResponsesIndexView(surveyID));
-	
+
 	var tableRowClickedCallback = function(e) {
 		navGroup.open(new ResponseShowWindow(e.responseID));
 	}
-	
 	var syncButton = Ti.UI.createButton({
 		systemButton : Ti.UI.iPhone.SystemButton.REFRESH
 	});
 	syncButton.addEventListener('click', function(e) {
-		Survey.syncResponses(surveyID);
+		survey = Survey.findOneById(surveyID);
+		survey.syncResponses();
 	});
 	self.rightNavButton = syncButton;
 
 	Ti.App.addEventListener('ResponsesIndexView:table_row_clicked', tableRowClickedCallback);
-	
+
 	self.addEventListener('close', function() {
 		Ti.App.removeEventListener('ResponsesIndexView:table_row_clicked', tableRowClickedCallback)
-	})
+	});
+	
+	var syncSuccessHandler = function() {
+		Ti.App.removeEventListener("survey.responses.sync.success", syncSuccessHandler);
+		navGroup.close(self);
+		alert("successfully uploaded responses!");
+	};
+
+	Ti.App.addEventListener("survey.responses.sync.success", syncSuccessHandler);
+
+	var syncErrorHandler = function() {
+		Ti.App.removeEventListener("survey.responses.sync.error", syncErrorHandler);
+		navGroup.close(self);
+		alert("error in uploading responses!");
+	};
+
+	Ti.App.addEventListener("survey.responses.sync.error", syncErrorHandler);
 
 	return self;
 }
