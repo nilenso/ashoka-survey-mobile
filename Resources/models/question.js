b95eb15e40ab6cd45e7a5ec9abd1fe9d7967e9b4
Timothy Andrew <mail@timothyandrew.net>
2012-10-04 12:29:59 +0530

    Store image_url for a question.

--- Resources/models/question.js
+++ Resources/models/question.js
@@ -5,23 +5,36 @@ var Question = new Ti.App.joli.model({
 		content : 'TEXT',
 		survey_id : 'INTEGER',
 		mandatory : 'INTEGER',
-		max_length : 'INTEGER'
+		max_length : 'INTEGER',
+		image_url : 'TEXT'
 	},
 
 	methods : {
 		createRecords : function(data, surveyID) {
 			var _ = require('lib/underscore')._;
 			var that = this;
+			var records = [];
 			_(data).each(function(question) {
 				var record = that.newRecord({
 					id : question.id,
 					content : question.content,
 					survey_id : surveyID,
 					max_length : question.max_length,
-					mandatory : question.mandatory
+					mandatory : question.mandatory,
+					image_url : question.image_url
 				});
 				record.save();
+				records.push(record);
 			});
+			return records;
+		},
+	},
+
+	objectMethods : {
+		fetchImage : function() {
+			if (this.image_url) {
+				Ti.API.info("IMAGE URL IS: " + this.image_url.toString());
+			}
 		}
 	}
 });
