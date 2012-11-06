abff1a52786e3381b5dd61de7919db2243c92ade
Srihari Sriraman <srihari@c42.in>
2012-11-06 16:30:03 +0530

    Fixed the display and reset of errors in ResponseNewView by using the module ResponseViewHelper

--- Resources/ui/common/responses/ResponsesNewView.js
+++ Resources/ui/common/responses/ResponsesNewView.js
@@ -5,22 +5,13 @@ function ResponsesNewView(surveyID) {
   var Survey = require('models/survey');
   var Response = require('models/response');
   var QuestionView = require('ui/common/questions/QuestionView');
+  var ResponseViewHelper = require('ui/common/responses/ResponseViewHelper');
+  var responseViewHelper = new ResponseViewHelper;
 
   var self = Ti.UI.createScrollView({
     layout : 'vertical',
     height : Titanium.UI.SIZE
   });
-
-  var generateLabelTextForQuestion = function(question, errorText) {
-    text = '';
-    text += question['content'];
-    text += question.mandatory ? ' *' : '';
-    text += question.max_length ? ' [' + question.max_length + ']' : '';
-    text += question.max_value ? ' (<' + question.max_value + ')' : '';
-    text += question.min_value ? ' (>' + question.min_value + ')' : '';
-    text += errorText ? '\n' + errorText : '';
-    return text;
-  }
 
   var survey = Survey.findOneById(surveyID);
   var questions = survey.firstLevelQuestions();
@@ -29,47 +20,9 @@ function ResponsesNewView(surveyID) {
     self.add(questionView);
   });
 
-  var resetErrors = function() {
-    _(questionViews(self)).each(function(fields, questionID) {
-      var question = Question.findOneById(questionID);
-      var labelText = generateLabelTextForQuestion(question);
-      fields.label.setText(labelText);
-      fields.label.setColor('#000000');
-    });
-  }
-  var displayErrors = function(responseErrors) {
-    resetErrors();
-    Ti.API.info("All the errors:" + responseErrors);
-    for (var answerErrors in responseErrors) {
-      Ti.API.info("Answer errors for:" + answerErrors);
-      for (var field in responseErrors[answerErrors]) {
-        var question_id = answerErrors;
-        var question = Question.findOneById(question_id);
-        var label = questionViews(self)[question_id].label;
-        var labelText = generateLabelTextForQuestion(question, responseErrors[question_id][field]);
-        label.setText(labelText);
-        label.setColor("red");
-        Ti.API.info(responseErrors[question_id][field]);
-      }
-    }
-  }
-  var questionViews = function(parentView) {
-    var foo = {}
-    _(parentView.getChildren()).each(function(view) {
-      if (view.type == 'question') {
-        foo[view.id] = {
-          'label' : _(view.children).first(),
-          'valueField' : _(view.children).last()
-        };
-        Ti.API.info("label and value" + _(view.children).first() + _(view.children).last());
-      }
-      _(foo).extend(questionViews(view));
-    });
-    return foo;
-  };
-
   var validateAndSaveAnswers = function(e, status) {
-    var answersData = _(questionViews(self)).map(function(fields, questionID) {
+    var questionViews = responseViewHelper.getQuestionViews(self);
+    var answersData = _(questionViews).map(function(fields, questionID) {
       Ti.API.info("questionid:" + questionID);
       Ti.API.info("content:" + fields['valueField'].getValue());
       return {
@@ -79,7 +32,7 @@ function ResponsesNewView(surveyID) {
     });
     responseErrors = Response.validate(answersData, status);
     if (!_.isEmpty(responseErrors)) {
-      displayErrors(responseErrors);
+      responseViewHelper.displayErrors(responseErrors, questionViews);
       alert("There were some errors in the response.");
     } else {
       Response.createRecord(surveyID, status, answersData);
