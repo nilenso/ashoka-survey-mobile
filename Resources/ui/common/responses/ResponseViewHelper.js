nilenso/ashoka-survey-mobile

commit 6060ea792b6899dde72134fed3f504bbac1a80ff

Remove the accumulator as it was causing some memory issues.
Get the views out of the scrollable view when we need to save/complete.

Resources/ui/common/responses/ResponseViewHelper.js
@@ -41,9 +41,14 @@ function ResponseViewHelper() {
 		}
 	};
 
-	var getQuestionViews = function(parentView) {
-		var foo = {}
-		_(parentView.getChildren()).each(function(view) {
+	var getQuestionViews = function(parent) {
+		var foo = {};
+		if(parent instanceof Array) {
+		  var views = _.chain(parent).map(function(scrollView) { return scrollView.children; }).flatten().value();
+		} else {
+		  var views = parent.children || [];
+		}
+		_(views).each(function(view) {
 			if (view.type == 'question') {
 				foo[view.id] = {
 					'label' : _(view.children).first(),
@@ -57,7 +62,7 @@ function ResponseViewHelper() {
 		return foo;
 	};
 
-	var paginate = function(questions, accumulator, scrollableView, buttons) {
+	var paginate = function(questions, scrollableView, buttons) {
 		var PAGE_SIZE = 6;
 		
 		var pagedQuestions = _.chain(questions).groupBy(function(a, b) {
@@ -71,7 +76,6 @@ function ResponseViewHelper() {
 
 			_(questions).each(function(question) {
 				var questionView = new QuestionView(question);
-				accumulator.add(questionView);
 				questionsView.add(questionView);
 			})
 			

Resources/ui/common/responses/ResponsesNewView.js
@@ -20,8 +20,6 @@ function ResponsesNewView(surveyID) {
 	var survey = Survey.findOneById(surveyID);
 	var questions = survey.firstLevelQuestions();
 	
-	var allQuestionViews = Ti.UI.createView();
-	
 	var saveButton = Ti.UI.createButton({
 		title : 'Save',
 		width : '48%'
@@ -32,7 +30,7 @@ function ResponsesNewView(surveyID) {
 		width : '48%'
 	});
 	
-	responseViewHelper.paginate(questions, allQuestionViews, scrollableView, [saveButton, completeButton]);
+	responseViewHelper.paginate(questions, scrollableView, [saveButton, completeButton]);
 
 	var getCurrentLocation = function() {
 		var location = {};
@@ -50,7 +48,7 @@ function ResponsesNewView(surveyID) {
 	};
 	
 	var validateAndSaveAnswers = function(e, status) {
-		var questionViews = responseViewHelper.getQuestionViews(allQuestionViews);
+		var questionViews = responseViewHelper.getQuestionViews(scrollableView.getChildren());
 		var answersData = _(questionViews).map(function(fields, questionID) {
 			Ti.API.info("questionid:" + questionID);
 			Ti.API.info("content:" + fields['valueField'].getValue());

commit 1551a1741910f7d40a4775477c9da78b9d5bb4a9

Fix bug introduced in 6060ea7 where answers weren't getting saved.

Resources/ui/common/responses/ResponseViewHelper.js
@@ -43,10 +43,10 @@ function ResponseViewHelper() {
 
 	var getQuestionViews = function(parent) {
 		var foo = {};
-		if(parent instanceof Array) {
+		if(_(parent).isArray()) {
 		  var views = _.chain(parent).map(function(scrollView) { return scrollView.children; }).flatten().value();
 		} else {
-		  var views = parent.children || [];
+		  var views = parent.getChildren() || [];
 		}
 		_(views).each(function(view) {
 			if (view.type == 'question') {

Resources/ui/common/responses/ResponsesNewView.js
@@ -48,7 +48,7 @@ function ResponsesNewView(surveyID) {
 	};
 	
 	var validateAndSaveAnswers = function(e, status) {
-		var questionViews = responseViewHelper.getQuestionViews(scrollableView.getChildren());
+		var questionViews = responseViewHelper.getQuestionViews(scrollableView.getViews());
 		var answersData = _(questionViews).map(function(fields, questionID) {
 			Ti.API.info("questionid:" + questionID);
 			Ti.API.info("content:" + fields['valueField'].getValue());
@@ -58,7 +58,7 @@ function ResponsesNewView(surveyID) {
 			}
 		});
 		var responseLocation = getCurrentLocation();
-		responseErrors = Response.validate(answersData, status);
+		var responseErrors = Response.validate(answersData, status);
 		if (!_.isEmpty(responseErrors)) {
 			responseViewHelper.displayErrors(responseErrors, questionViews);
 			alert("There were some errors in the response.");