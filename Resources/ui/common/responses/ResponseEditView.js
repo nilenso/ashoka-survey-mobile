0a54ed981dbce4c3e626a6b345b8f721c841589c
Timothy Andrew <mail@timothyandrew.net>
2012-11-20 13:53:58 +0530

    add pagination to responses#edit

--- Resources/ui/common/responses/ResponseEditView.js
+++ Resources/ui/common/responses/ResponseEditView.js
@@ -1,71 +1,66 @@
 //All the questoin in a survey
 function ResponseEditView(responseID) {
-  var _ = require('lib/underscore')._;
-  var Question = require('models/question');
-  var Answer = require('models/answer');
-  var Response = require('models/response');
-  var Survey = require('models/survey');
-  var QuestionView = require('ui/common/questions/QuestionView');
-  var ResponseViewHelper = require('ui/common/responses/ResponseViewHelper');
-  var responseViewHelper = new ResponseViewHelper;
+	var _ = require('lib/underscore')._;
+	var Question = require('models/question');
+	var Answer = require('models/answer');
+	var Response = require('models/response');
+	var Survey = require('models/survey');
+	var QuestionView = require('ui/common/questions/QuestionView');
+	var ResponseViewHelper = require('ui/common/responses/ResponseViewHelper');
+	var responseViewHelper = new ResponseViewHelper;
 
-  var self = Ti.UI.createScrollView({
-    layout : 'vertical'
-  });
-  
-  var response = Response.findOneById(responseID);
-  var survey = Survey.findOneById(response.survey_id);
-  var questions = survey.firstLevelQuestions();
-  _(questions).each(function(question) {
-    var answer = response.answerForQuestion(question.id); 
-    var questionView = new QuestionView(question, answer);
-    self.add(questionView);
-  });
+	var self = Ti.UI.createScrollableView({
+		showPagingControl : true
+	});
 
-  var validateAndUpdateAnswers = function(e, status) {
-    var questionViews = responseViewHelper.getQuestionViews(self);
-    var answersData = _(questionViews).map(function(fields, questionID) {
-      Ti.API.info("questionid:" + questionID);
-      Ti.API.info("content:" + fields['valueField'].getValue());
-      return {
-        'id' : fields.answerID,
-        'question_id' : questionID,
-        'content' : fields.valueField.getValue()
-      };
-    });
-    responseErrors = Response.validate(answersData, status);
-    if (!_.isEmpty(responseErrors)) {
-      responseViewHelper.displayErrors(responseErrors, questionViews);
-      alert("There were some errors in the response.");
-    } else {
-      var response = Response.findOneById(responseID);
-      response.update(status, answersData);
-      self.fireEvent('ResponsesEditView:savedResponse');
-    }
-  };
+	var saveButton = Ti.UI.createButton({
+		title : 'Save',
+		width : '48%'
+	});
 
+	var completeButton = Ti.UI.createButton({
+		title : 'Complete',
+		width : '48%'
+	});
 
-  var saveButton = Ti.UI.createButton({
-    title : 'Save',
-    width : '48%'
-  });
+	var response = Response.findOneById(responseID);
+	var survey = Survey.findOneById(response.survey_id);
+	var questions = survey.firstLevelQuestions();
 
-  var completeButton = Ti.UI.createButton({
-    title : 'Complete',
-    width : '48%'
-  });
- 
-  self.add(saveButton);
-  self.add(completeButton);
+	var allQuestionViews = Ti.UI.createView();
 
-  completeButton.addEventListener('click', function(event) {
-    validateAndUpdateAnswers(event, "complete");
-  });
-  saveButton.addEventListener('click', function(event) {
-    validateAndUpdateAnswers(event, "incomplete");
-  });
-  
-  return self;
+	responseViewHelper.paginate(questions, allQuestionViews, self, [saveButton, completeButton]);
+
+	var validateAndUpdateAnswers = function(e, status) {
+		var questionViews = responseViewHelper.getQuestionViews(allQuestionViews);
+		var answersData = _(questionViews).map(function(fields, questionID) {
+			Ti.API.info("questionid:" + questionID);
+			Ti.API.info("content:" + fields['valueField'].getValue());
+			return {
+				'id' : fields.answerID,
+				'question_id' : questionID,
+				'content' : fields.valueField.getValue()
+			};
+		});
+		responseErrors = Response.validate(answersData, status);
+		if (!_.isEmpty(responseErrors)) {
+			responseViewHelper.displayErrors(responseErrors, questionViews);
+			alert("There were some errors in the response.");
+		} else {
+			var response = Response.findOneById(responseID);
+			response.update(status, answersData);
+			self.fireEvent('ResponsesEditView:savedResponse');
+		}
+	};
+
+	completeButton.addEventListener('click', function(event) {
+		validateAndUpdateAnswers(event, "complete");
+	});
+	saveButton.addEventListener('click', function(event) {
+		validateAndUpdateAnswers(event, "incomplete");
+	});
+
+	return self;
 }
 
 module.exports = ResponseEditView;
