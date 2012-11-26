2cfa6d205d308158c52215839603758c8cd67782
Timothy Andrew <mail@timothyandrew.net>
2012-11-26 18:07:49 +0530

    Basic login implemented. The cookie that rails returned is stored if login is successful.

--- Resources/ui/common/LoginView.js
+++ Resources/ui/common/LoginView.js
@@ -1,6 +1,7 @@
 function LoginView() {
 	
-	var loginUrl = Ti.App.Properties.getString('server_url') + '/auth/user_owner';
+	var loginUrl = Ti.App.Properties.getString('server_url') + '/api/login';
+	
 	var self = Titanium.UI.createView({
 		layout : 'vertical'
 	});
@@ -25,6 +26,28 @@ function LoginView() {
 	self.add(emailField);
 	self.add(passwordField);
 	self.add(loginButton);
+	
+	loginButton.addEventListener('click', function(){
+	  var email = emailField.getValue();
+	  var password = passwordField.getValue();
+	  var client = Ti.Network.createHTTPClient();
+	  client.autoRedirect = false;
+	  
+	  client.onload = function() {
+	    var cookie = this.getResponseHeader('Set-Cookie');
+	    Ti.App.Properties.setString('auth_cookie', cookie);
+	    //FireEvent to close.
+	  } 
+	  
+	  client.onerror = function() {
+	    alert("Login failed, sorry!");
+	  } 
+	  client.open('POST', loginUrl);
+	  client.send({
+	    username: email,
+	    password: password
+	  });
+	});
 	
 	
 	return self;
