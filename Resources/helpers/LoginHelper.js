fd9b688d1e416b53fb0a20b322d485f39c744d86
Timothy Andrew <mail@timothyandrew.net>
2012-11-27 14:46:28 +0530

    Expire the session (cookie) in one hour after login.

--- Resources/helpers/LoginHelper.js
+++ Resources/helpers/LoginHelper.js
@@ -2,12 +2,21 @@
 var LoginHelper = {
   loggedIn : function() {
     var cookie = Ti.App.Properties.getString('auth_cookie');
+    var cookieCreatedAt = Ti.App.Properties.getString('auth_cookie_created_at');
+    
     if (!cookie)
       return false;
+      
+    if(new Date() - new Date(cookieCreatedAt) > 3600000) { //Expire the cookie after 1 hour.
+      alert("Your session has expired. You need to login again to fetch or sync any data.");
+      LoginHelper.expireSession();
+      return false;  
+    }
+        
     return true;
   },
 
-  logout : function() {
+  logout : function(clearDB) {
     var createConfirmDialog = function() {
       var confirmDialog = Ti.UI.createAlertDialog({
         title : "Logout",
@@ -22,7 +31,7 @@ var LoginHelper = {
       if (e.index === e.source.cancel) {
         Ti.API.info('The logout was cancelled');
       } else {
-        Ti.App.Properties.setString('auth_cookie', null);
+        LoginHelper.expireSession();
         var DatabaseHelper = require("helpers/DatabaseHelper");
         DatabaseHelper.clearDownloadedData();
         Ti.App.fireEvent('settings.refreshSurveys');
@@ -30,7 +39,13 @@ var LoginHelper = {
       }
     });
     confirmDialog.show();
+  },
+  
+  expireSession : function() {
+    Ti.App.Properties.setString('auth_cookie', null);
+    Ti.App.Properties.setString('auth_cookie_created_at', null); 
   }
+  
 }
 
 module.exports = LoginHelper;
