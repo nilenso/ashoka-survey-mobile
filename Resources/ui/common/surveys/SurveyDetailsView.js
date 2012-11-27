a7589582c03df1d74ca73d70c2de6ca923ea8fe0
Srihari Sriraman <srihari@c42.in>
2012-11-27 19:35:21 +0530

    Styled the survey details view

--- Resources/ui/common/surveys/SurveyDetailsView.js
+++ Resources/ui/common/surveys/SurveyDetailsView.js
@@ -1,63 +1,93 @@
 //A single survey
 function SurveyDetailsView(surveyID) {
-	var _ = require('lib/underscore')._;
-	var Survey = require('models/survey');
+  var _ = require('lib/underscore')._;
+  var Survey = require('models/survey');
   var TopLevelView = require('ui/common/components/TopLevelView');
+  var SeparatorView = require('ui/common/components/SeparatorView');
+  var Palette = require('ui/common/components/Palette');
 
-	var convertSurveyDataForTable = function() {
-		var attrs = Survey.findOneById(surveyID)
-		return [{
-			header : 'Name',
-			title : attrs['name']
-		}, {
-			header : 'Description',
-			title : attrs['description']
-		}, {
-			header : 'Expires On',
-			title : attrs['expiry_date']
-		}];
-	}
-	
-	var self = new TopLevelView('Survey Details');
+  var survey = Survey.findOneById(surveyID);
+  var detailsView = Ti.UI.createView({
+    top : '48dip',
+    backgroundColor : Palette.SECONDARY_COLOR_LIGHT,
+    layout : 'vertical',
+  });
+  var surveyNameLabel = Ti.UI.createLabel({
+    text : survey.name,
+    color : Palette.PRIMARY_COLOR,
+    left : '5dip',
+    font : {
+      fontSize : '15dip'
+    }
+  });
+  var surveyDescriptionLabel = Ti.UI.createLabel({
+    text : survey.description,
+    color : Palette.PRIMARY_COLOR,
+    left : '5dip',
+    font : {
+      fontSize : '10dip'
+    }
+  });
+  var surveyExpiryLabel = Ti.UI.createLabel({
+    text : 'Expires on: ' + survey.expiry_date,
+    color : Palette.PRIMARY_COLOR,
+    left : '5dip',
+    font : {
+      fontSize : '10dip'
+    }
+  });
+  detailsView.add(surveyNameLabel);
+  detailsView.add(surveyDescriptionLabel);
+  detailsView.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, '5dip'));
+  detailsView.add(surveyExpiryLabel);
 
-	// now assign that array to the table's data property to add those objects as rows
-	var table = Titanium.UI.createTableView({
-	  top : '43dip',
-		data : convertSurveyDataForTable()
-	});
+  var self = new TopLevelView('Survey Details');
 
-	var createResponseButton = Ti.UI.createButton({
-		title : 'Add Response',
-		width : '100%'
-	});
+  var createResponseButton = Ti.UI.createButton({
+    title : 'Add Response',
+    backgroundColor : Palette.PRIMARY_COLOR,
+    backgroundSelectedColor : Palette.PRIMARY_COLOR_LIGHT,
+    backgroundFocusedColor : Palette.PRIMARY_COLOR_LIGHT,
+    color : Palette.SECONDARY_COLOR_LIGHT,
+    width : '80%',
+    font : {
+      size : '20dip'
+    },
+    height : '30dip'
+  });
 
-	createResponseButton.addEventListener('click', function(e) {
-		self.fireEvent('SurveyDetailsView:createResponse', {
-			surveyID : surveyID
-		});
-	});
+  var responsesIndexButton = Ti.UI.createButton({
+    title : 'See all Responses',
+    backgroundColor : Palette.PRIMARY_COLOR,
+    backgroundSelectedColor : Palette.PRIMARY_COLOR_LIGHT,
+    backgroundFocusedColor : Palette.PRIMARY_COLOR_LIGHT,
+    color : Palette.SECONDARY_COLOR_LIGHT,
+    width : '80%',
+    font : {
+      size : '20dip'
+    },
+    height : '30dip'
+  });
 
-	var responsesIndexButton = Ti.UI.createButton({
-		title : 'See all Responses',
-		width : '100%'
-	});
+  detailsView.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, '5dip'));
+  detailsView.add(createResponseButton);
+  detailsView.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, '5dip'));
+  detailsView.add(responsesIndexButton);
 
-	responsesIndexButton.addEventListener('click', function(e) {
-		self.fireEvent('SurveyDetailsView:responsesIndex', {
-			surveyID: surveyID
-		});
-	});
+  createResponseButton.addEventListener('click', function(e) {
+    self.fireEvent('SurveyDetailsView:createResponse', {
+      surveyID : surveyID
+    });
+  });
 
-	var buttonsView = Ti.UI.createView({
-		layout : 'vertical'
-	});
-	buttonsView.add(createResponseButton);
-	buttonsView.add(responsesIndexButton);
+  responsesIndexButton.addEventListener('click', function(e) {
+    self.fireEvent('SurveyDetailsView:responsesIndex', {
+      surveyID : surveyID
+    });
+  });
 
-	table.setFooterView(buttonsView);
-
-	self.add(table);
-	return self;
+  self.add(detailsView);
+  return self;
 }
 
 module.exports = SurveyDetailsView;
