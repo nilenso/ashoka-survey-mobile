b3534ae35a86a50278d5b2106e1bb9093579dfeb
Srihari Sriraman <srihari@c42.in>
2012-11-27 17:38:48 +0530

    Separated the survey row to a different file SurevyRowView

---
+++ Resources/ui/common/surveys/SurveyRowView.js
@@ -0,0 +1,46 @@
+var Survey = require('models/survey');
+var Palette = require('ui/common/components/Palette');
+
+function SurveysRowView(survey) {
+  var self = Ti.UI.createTableViewRow({
+    backgroundColor : Palette.SECONDARY_COLOR_LIGHT,
+    hasDetail : true,
+    surveyID : survey.id,
+    layout : 'vertical'
+  });
+  var surveyNameLabel = Ti.UI.createLabel({
+    text : survey.name,
+    color : Palette.PRIMARY_COLOR,
+    left : '5dip',
+    font : {
+      fontSize : '15dip'
+    }
+  });
+  var surveyInfoView = Ti.UI.createView({
+    width : '100%'
+  });
+
+  var responseCountLabel = Ti.UI.createLabel({
+    text : '[' + survey.responseCount() + ']',
+    color : Palette.PRIMARY_COLOR,
+    right : '5dip',
+    font : {
+      fontSize : '10dip'
+    }
+  });
+  var expiryDateLabel = Ti.UI.createLabel({
+    text : 'Expires on: ' + survey.expiry_date,
+    color : Palette.PRIMARY_COLOR,
+    left : '5dip',
+    font : {
+      fontSize : '10dip'
+    }
+  });
+  self.add(surveyNameLabel);
+  surveyInfoView.add(expiryDateLabel);
+  surveyInfoView.add(responseCountLabel);
+  self.add(surveyInfoView);
+  return (self);
+}
+
+module.exports = SurveysRowView;
