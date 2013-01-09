ded15a1477b6ae52f1c8583e1bdd6bd5c446f7b7
Nivedita <nivedita@c42.in>
2013-01-09 16:07:17 +0530

    Added a vertical margin for question numbers on response new and edit page

--- Resources/ui/common/questions/QuestionView.js
+++ Resources/ui/common/questions/QuestionView.js
@@ -12,18 +12,37 @@ var Measurements = require('ui/common/components/Measurements');
 function QuestionView(question, answer, response, number) {
   var self = Ti.UI.createView({
     backgroundColor : Palette.SECONDARY_COLOR_LIGHT,
-    layout : 'vertical',
+    layout : 'horizontal',
     type : question.type ? 'question' : 'category',
     id : question.id,
     height : Titanium.UI.SIZE,
     answerID : answer ? answer.id : null
   });
 
-  var questionText = number + '. ';
-  questionText += question['content'];
+  var questionWrapper = Ti.UI.createView({
+    height : Titanium.UI.SIZE,
+    layout : 'vertical',
+    right : Measurements.PADDING_X_SMALL,
+    width : '90%'
+  });
+  var questionNumber = Ti.UI.createLabel({
+    text : number + ".",
+    left : Measurements.PADDING_X_SMALL,
+    width : '5%',
+    color : Palette.PRIMARY_COLOR,
+    font : {
+      fontSize : Measurements.FONT_MEDIUM
+    },
+    top : Measurements.PADDING_MEDIUM
+  });
+
+  self.add(questionNumber);
+  self.add(questionWrapper);
+
+  var questionText = question['content'];
   questionText += question.mandatory ? ' *' : '';
 
-  self.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, '10dip'));
+  questionWrapper.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, '10dip'));
 
   var labelsView = Ti.UI.createView({
     layout : 'vertical',
@@ -48,8 +67,8 @@ function QuestionView(question, answer, response, number) {
 
   var errorsLabel;
 
-  self.add(labelsView);
-  self.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, Measurements.PADDING_SMALL));
+  questionWrapper.add(labelsView);
+  questionWrapper.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, Measurements.PADDING_SMALL));
 
   if (question.image_url) {
     var imageView = Ti.UI.createImageView({
@@ -57,7 +76,7 @@ function QuestionView(question, answer, response, number) {
       height : 100,
       image : Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, question.id.toString())
     });
-    self.add(imageView);
+    questionWrapper.add(imageView);
   }
 
   var valueField;
@@ -80,8 +99,8 @@ function QuestionView(question, answer, response, number) {
     valueField = new BasicQuestionView(question, content, constraintsText);
   }
 
-  self.add(valueField);
-  self.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, '10dip'));
+  questionWrapper.add(valueField);
+  questionWrapper.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, '10dip'));
 
   self.setError = function(errorText) {
     self.has_error = true;
