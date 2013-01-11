58ba6bd6d63cde502fb0e3215c42116e8b6c4132
Smit Shah <who828@gmail.com>
2013-01-11 12:29:03 +0530

    Delete the image when clearing the imageView

--- Resources/ui/common/questions/PhotoQuestionView.js
+++ Resources/ui/common/questions/PhotoQuestionView.js
@@ -4,105 +4,111 @@ var ButtonView = require('ui/common/components/ButtonView');
 //BasicQuestionView Component Constructor
 function PhotoQuestionView(question, image) {
 
-	var self = Ti.UI.createView({
-		layout : 'vertical',
-		height : Titanium.UI.SIZE
-	});
+  var self = Ti.UI.createView({
+    layout : 'vertical',
+    height : Titanium.UI.SIZE
+  });
 
-	var pictureButton = new ButtonView('Take a Picture', { 'width' : '48%' });
-	var path, imageView, clearPictureButton;
-	
-	var clearImage = function() {
-	  path = null;
+  var pictureButton = new ButtonView('Take a Picture', { 'width' : '48%' });
+  var path, imageView, clearPictureButton;
+
+  var deleteImage = function() {
+    var imageFile = Ti.Filesystem.getFile(path);
+    imageFile.deleteFile();
+  };
+
+  var clearImage = function() {
     if(imageView) {
       self.remove(imageView);
+      deleteImage();
+      path = null;
       imageView = null;
     }
     if(clearPictureButton){
       self.remove(clearPictureButton);
       clearPictureButton = null;
     }
-	}
+  };
 
-	var addImageView = function(image) {
-		imageView = Ti.UI.createImageView({
-			width : 100,
-			height : 100,
-			image : image
-		});
-		self.add(imageView);
+  var addImageView = function(image) {
+    imageView = Ti.UI.createImageView({
+      width : 100,
+      height : 100,
+      image : image
+    });
+    self.add(imageView);
 
-		clearPictureButton = new ButtonView('Clear the Picture', { 'width' : '48%' });
+    clearPictureButton = new ButtonView('Clear the Picture', { 'width' : '48%' });
 
-		self.add(clearPictureButton);
+    self.add(clearPictureButton);
 
-		clearPictureButton.addEventListener('click', function() {
-			clearImage();
-		});
-	};
+    clearPictureButton.addEventListener('click', function() {
+      clearImage();
+    });
+  };
 
-	self.add(pictureButton);
+  self.add(pictureButton);
 
-	pictureButton.addEventListener('click', function() {
-		Ti.Media.showCamera({
-			success : function(event) {
-				if (event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
-					_(self.getChildren()).each(function(childView) {
-						if (childView != pictureButton)
-							self.remove(childView);
-					});
-					addImageView(event.media);
-					var filename = "image_" + (new Date()).valueOf() + ".jpg";
-					var file = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, filename);
-					file.write(event.media);
-					event.media = null;
-					resize(file);
+  pictureButton.addEventListener('click', function() {
+    Ti.Media.showCamera({
+      success : function(event) {
+        if (event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
+          _(self.getChildren()).each(function(childView) {
+            if (childView != pictureButton)
+              self.remove(childView);
+          });
+          addImageView(event.media);
+          var filename = "image_" + (new Date()).valueOf() + ".jpg";
+          var file = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, filename);
+          file.write(event.media);
+          event.media = null;
+          resize(file);
 
-				} else {
-					alert("got the wrong type back :" + event.mediaType);
-				}
-			},
-			cancel : function(event) {
-			},
-			error : function(event) {
-				Ti.API.info("camera error");
-			},
-			autohide : true,
-			mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO],
-			allowEditing: true
-		});
-	});
-	
-	var resize = function(file) {
-	  var ImageFactory = require('ti.imagefactory');
-	  var image = file.read();
-	  var new_width = 1000;
-	  var new_height = (image.height / image.width) * new_width;
-	  try {
-	    file.write(ImageFactory.imageAsResized(image, { width: new_width, height : new_height, quality: 0.7 }));
-	  } catch(err) {
-	    Ti.API.info("ERROR SAVING IMAGE " + err);
-	    clearImage();
-	    alert("Your phone has run out of memory.\nPlease close other running applications and try again.");
-	  }
-	  path = file.nativePath;
-	};
+        } else {
+          alert("got the wrong type back :" + event.mediaType);
+        }
+      },
+      cancel : function(event) {
+      },
+      error : function(event) {
+        Ti.API.info("camera error");
+      },
+      autohide : true,
+      mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO],
+      allowEditing: true
+    });
+  });
 
-	if (image) {
-		var imageFile = Ti.Filesystem.getFile(image);
-		addImageView(imageFile.read());
-		path = imageFile.nativePath;
-	}
+  var resize = function(file) {
+    var ImageFactory = require('ti.imagefactory');
+    var image = file.read();
+    var new_width = 1000;
+    var new_height = (image.height / image.width) * new_width;
+    try {
+      file.write(ImageFactory.imageAsResized(image, { width: new_width, height : new_height, quality: 0.7 }));
+    } catch(err) {
+      Ti.API.info("ERROR SAVING IMAGE " + err);
+      clearImage();
+      alert("Your phone has run out of memory.\nPlease close other running applications and try again.");
+    }
+    path = file.nativePath;
+  };
 
-	self.getValue = function() {
-		if (path) {
-			Ti.API.info("Photo for photo question is " + path);
-			return path;
-		}
-		return null;
-	};
+  if (image) {
+    var imageFile = Ti.Filesystem.getFile(image);
+    addImageView(imageFile.read());
+    path = imageFile.nativePath;
+  }
 
-	return self;
+  self.getValue = function() {
+    if (path) {
+      Ti.API.info("Photo for photo question is " + path);
+      return path;
+    }
+    return null;
+  };
+
+  return self;
 }
 
 module.exports = PhotoQuestionView;
