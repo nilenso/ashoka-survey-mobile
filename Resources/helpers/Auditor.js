bcd4a9d0eb475f70d10bd15b87e4f7229ee324b0
Nivedita <nivedita@c42.in>
2013-01-30 14:25:56 +0530

    Do a PUT request if the device's audit file has been sync'd once.

--- Resources/helpers/Auditor.js
+++ Resources/helpers/Auditor.js
@@ -5,6 +5,13 @@ var Auditor = function() {
   var auditFileName = "audit-" + deviceId + ".log";
   var auditUrl = Ti.App.Properties.getString('server_url') + '/api/audits';
 
+  self.firstAudit = Ti.App.Properties.getString('firstAudit') === null ? true : false;
+
+  setNotFirstAudit = function(){
+    self.firstAudit = false;
+    Ti.App.Properties.setString('firstAudit', false);
+  };
+
   var getFile = function() {
     return Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, auditFileName);
   };
@@ -31,22 +38,39 @@ var Auditor = function() {
     return content.text;
   };
 
+  var truncateAuditFile = function() {
+    var file = getFile();
+    file.write("");
+    file = null;
+  };
+
+  var params = function() {
+    var params = {
+      device_id : deviceId,
+      content :  readFile()
+    };
+    if(self.firstAudit)
+      params['platform_data'] = getPlatformData();
+    return params;
+  };
+
   self.writeIntoAuditFile = function(text) {
     var file = getFile();
-    file.write("\n" + text, true);
+    file.write("\n" + (new Date()).toString(), true);
+    file.write(" : " + text, true);
     file = null;
   };
 
   self.sendAuditFile = function() {
     var client = Ti.Network.createHTTPClient();
-    client.onload = function(){alert("audit send success");};
+    var requestMethod = self.firstAudit ? "POST" : "PUT";
+    client.onload = function(){
+      truncateAuditFile();
+      setNotFirstAudit();
+    };
     client.onerror = function(){alert("error");};
-    client.open("POST", auditUrl);
-    client.send({
-      device_id : deviceId,
-      content :  readFile(),
-      platform_data : getPlatformData()
-    });
+    client.open(requestMethod, auditUrl);
+    client.send(params());
   };
 
   return self;
