c413811428ea45271c279020a55c8c96be971250
Nivedita <nivedita@c42.in>
2013-01-31 12:22:40 +0530

    Added a remember me checkbox in loginView. Saved the password in Ti.App.Properties if remember me is checked

--- Resources/ui/common/LoginView.js
+++ Resources/ui/common/LoginView.js
@@ -28,6 +28,30 @@ function LoginView() {
     passwordMask : true
   });
 
+  var rememberMeView = Ti.UI.createView({
+    layout : 'horizontal',
+    height : Titanium.UI.SIZE,
+    width : '80%'
+  });
+  var size = Ti.Platform.displayCaps.platformHeight * 0.05;
+  var rememberMeCheckbox = Ti.UI.createSwitch({
+    value : false,
+    style : Ti.UI.Android.SWITCH_STYLE_CHECKBOX,
+    height : size,
+    width : size,
+    left : Measurements.PADDING_X_SMALL,
+    titleOn : "",
+    titleOff : ""
+  });
+  var rememberMeLabel = Ti.UI.createLabel({
+    color : Palette.PRIMARY_COLOR,
+    text : L('remember_me'),
+    left : Measurements.PADDING_BIG,
+    font : {
+      fontSize : Measurements.FONT_MEDIUM
+    }
+  });
+
   var loginButton = new ButtonView('Login', {
     width : '60%'
   });
@@ -63,10 +87,12 @@ function LoginView() {
       emailField.setValue(oldEmail);
   }();
 
-
   self.add(activityIndicator);
   self.add(emailField);
   self.add(passwordField);
+  rememberMeView.add(rememberMeCheckbox);
+  rememberMeView.add(rememberMeLabel);
+  self.add(rememberMeView);
   self.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, Measurements.PADDING_SMALL));
   self.add(loginButton);
 
@@ -74,17 +100,19 @@ function LoginView() {
     var email = emailField.getValue().trim();
     var old_email = Ti.App.Properties.getString('email');
     var password = passwordField.getValue();
+    var rememberMe = rememberMeCheckbox.getValue();
+
     if (old_email && (email.toLowerCase() !== old_email.toLowerCase())) {
       var confirmDialog = new ConfirmDialog(L("login_menu"), L("login_confirm_dialog"), onConfirm = function(e) {
         var DatabaseHelper = require("helpers/DatabaseHelper");
         DatabaseHelper.clearDownloadedData();
         activityIndicator.show();
-        loginHelper.login(email, password, topLevelView);
+        loginHelper.login(email, password, rememberMe, topLevelView);
       });
       confirmDialog.show();
     } else {
       activityIndicator.show();
-      loginHelper.login(email, password, topLevelView);
+      loginHelper.login(email, password, rememberMe, topLevelView);
     }
   });
 
