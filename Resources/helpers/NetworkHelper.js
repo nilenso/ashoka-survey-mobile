921497646eb0757f9384a9c493af020ec1d7b76a
Srihari Sriraman <srihari@c42.in>
2013-02-01 12:45:18 +0530

    Added an activity indicator around the ping

--- Resources/helpers/NetworkHelper.js
+++ Resources/helpers/NetworkHelper.js
@@ -1,5 +1,10 @@
 var loggedIn = require('helpers/LoginHelper').loggedIn;
 var loginHelper = require('helpers/LoginHelper');
+var activityIndicator = Ti.UI.Android.createProgressIndicator({
+  message : L('reaching_server'),
+  location : Ti.UI.Android.PROGRESS_INDICATOR_DIALOG,
+  type : Ti.UI.Android.PROGRESS_INDICATOR_INDETERMINANT
+});
 
 var NetworkHelper = {
   pingSurveyWebWithLoggedInCheck : function(success, error) {
@@ -16,17 +21,26 @@ var NetworkHelper = {
   pingSurveyWebWithoutLoggedInCheck : function(success, error) {
     if(!networkOnlineCheck(error)) return;
     var client = Ti.Network.createHTTPClient({
-      onload : success,
-      onerror : error ||
-      function() {
-        Ti.App.fireEvent('network.server.unreachable');
-        alert(L("check_internet"));
+      onload : function(){
+        activityIndicator.hide();
+        success();
+      },
+      onerror : function(){
+        activityIndicator.hide();
+        if(error) {
+          error.call();
+        } else {
+          Ti.App.fireEvent('network.server.unreachable');
+          alert(L("check_internet"));
+        }
       },
       timeout : 5000
     });
 
     client.open('HEAD', Ti.App.Properties.getString('server_url'));
     client.send();
+
+    activityIndicator.show();
   },
 
   networkOnlineCheck : function (error) {
