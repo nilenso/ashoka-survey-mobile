49923567bec49821d1df487ac123ee08eef192b1
Nivedita <nivedita@c42.in>
2013-02-13 15:19:14 +0530

    Saving category_id with each record created.

--- Resources/models/record.js
+++ Resources/models/record.js
@@ -6,14 +6,19 @@ var Record = new Ti.App.joli.model({
   columns : {
     id : 'INTEGER PRIMARY KEY',
     response_id : 'INTEGER',
+    category_id : 'INTEGER',
     web_id : 'INTEGER'
   },
 
   methods : {
     createRecord : function(recordData, responseID){
+      var Question = require('models/question');
+      var question = Question.findOneById(recordData[0]['question_id']);
+      var multiRecordCategoryID = question.category_id;
       Ti.API.info("Creating a record");
       var record = this.newRecord({
-        response_id : responseID
+        response_id : responseID,
+        category_id : multiRecordCategoryID
       });
       record.save();
       Ti.API.info(record.id);
