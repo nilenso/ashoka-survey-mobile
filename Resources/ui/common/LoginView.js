e82d1cfd24c6e4fc8b3e42f07355e7eb0ac36300
Nivedita <nivedita@c42.in>
2013-03-05 16:41:58 +0530

    Removed remember me checkbox from LoginView. Changed the login message.

--- Resources/ui/common/LoginView.js
+++ Resources/ui/common/LoginView.js
@@ -28,30 +28,6 @@ function LoginView() {
     passwordMask : true
   });
 
-  var rememberMeView = Ti.UI.createView({
-    layout : 'horizontal',
-    height : Titanium.UI.SIZE,
-    width : '80%'
-  });
-  var size = Ti.Platform.displayCaps.platformHeight * 0.05;
-  var rememberMeCheckbox = Ti.UI.createSwitch({
-    value : false,
-    style : Ti.UI.Android.SWITCH_STYLE_CHECKBOX,
-    height : size,
-    width : size,
-    left : Measurements.PADDING_X_SMALL,
-    titleOn : "",
-    titleOff : ""
-  });
-  var rememberMeLabel = Ti.UI.createLabel({
-    color : Palette.PRIMARY_COLOR,
-    text : L('remember_me'),
-    left : Measurements.PADDING_BIG,
-    font : {
-      fontSize : Measurements.FONT_MEDIUM
-    }
-  });
-
   var loginButton = new ButtonView('Login', {
     width : '60%'
   });
@@ -65,9 +41,6 @@ function LoginView() {
 
   self.add(emailField);
   self.add(passwordField);
-  rememberMeView.add(rememberMeCheckbox);
-  rememberMeView.add(rememberMeLabel);
-  self.add(rememberMeView);
   self.add(new SeparatorView(Palette.SECONDARY_COLOR_LIGHT, Measurements.PADDING_SMALL));
   self.add(loginButton);
 
@@ -75,7 +48,6 @@ function LoginView() {
     var email = emailField.getValue().trim();
     var old_email = Ti.App.Properties.getString('email');
     var password = passwordField.getValue();
-    var rememberMe = rememberMeCheckbox.getValue();
 
     if (old_email && (email.toLowerCase() !== old_email.toLowerCase())) {
       var confirmDialog = new ConfirmDialog(L("login_menu"), L("login_confirm_dialog"), onConfirm = function(e) {
