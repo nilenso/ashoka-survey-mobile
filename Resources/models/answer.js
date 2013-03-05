ecfe97380de24288a7ce36c47e0bdf42ae1a6403
Nivedita <nivedita@c42.in>
2013-03-05 18:37:15 +0530

    Fixed the mandatory check for Rating questions.

--- Resources/models/answer.js
+++ Resources/models/answer.js
@@ -45,7 +45,7 @@ var Answer = new Ti.App.joli.model({
     validate : function(answerData, status) {
       var question = Question.findOneById(answerData.question_id);
       var errors = {};
-      if (answerData.content && !_.isEmpty(answerData.content)) {
+      if (answerData.content && !_.isEmpty(answerData.content.toString())) {
         if (question.max_length && (answerData.content.length > question.max_length))
           errors['max_length'] = L("error_max_length");
         if (question.min_value && answerData.content < question.min_value)
