dee0e5249ef9768df3bd01dfb0cbe999b05333d4
Timothy Andrew <mail@timothyandrew.net>
2013-03-07 16:40:01 +0530

    Cache the sub questions for a CategoryView.

--- Resources/ui/common/questions/CategoryView.js
+++ Resources/ui/common/questions/CategoryView.js
@@ -7,6 +7,8 @@ function CategoryView(category, response, number, pageNumber, recordID) {
     layout : 'vertical',
     height : Titanium.UI.SIZE
   });
+  
+  var childrenViews = null;
 
   Ti.API.info("Showing sub questions for" + category.content);  
 
@@ -15,12 +17,22 @@ function CategoryView(category, response, number, pageNumber, recordID) {
   };
   
   self.getSubQuestions = function() {
+    if(childrenViews) { 
+      return _.chain(childrenViews).map(function(view){
+        return _([view, view.getSubQuestions()]).compact();
+      }).flatten().value();
+    }
+    
+    childrenViews = [];
+    
+    
     var QuestionView = require('ui/common/questions/QuestionView');
     var subQuestions = category.firstLevelSubQuestions();
     return _(subQuestions).map(function(subQuestion, index) {
       var subQuestionAnswer = response ? response.answerForQuestion(subQuestion.id, recordID) : null;
       var subQuestionNumber = number + '.' + (index + 1);
       var questionView = (new QuestionView(subQuestion, subQuestionAnswer, response, subQuestionNumber, null, pageNumber, recordID));
+      childrenViews.push(questionView);
       return _([questionView]).union(questionView.getSubQuestions());
     });    
   };
