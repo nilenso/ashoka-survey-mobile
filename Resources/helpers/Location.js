4df3c3fee315458d1655881ee550e1a7f1a39e52
Timothy Andrew <mail@timothyandrew.net>
2013-03-19 13:49:12 +0530

    Use simple mode for Location.js.

--- Resources/helpers/Location.js
+++ Resources/helpers/Location.js
@@ -4,71 +4,68 @@ var location = {};
 
 function firstLocation(e) {
   if (e.success === undefined || e.success) {
-    if (typeof (callback) === 'function') {
+    if ( typeof (callback) === 'function') {
       if (timeout) clearTimeout(timeout);
       setLocation(e);
-      log();      
-      callback(location);      
+      log();
+      callback(location);
       Ti.Geolocation.removeEventListener("location", firstLocation);
-      
+
       // Might get more accurate readings if we leave a listener open.
-      Ti.Geolocation.addEventListener("location", subsequentLocations); 
+      Ti.Geolocation.addEventListener("location", subsequentLocations);
     }
   }
-} 
+}
 
 function subsequentLocations(e) {
-  if (e.success === undefined || e.success) {
-    if (typeof (callback) === 'function') {      
-      // Close the listener after getting a GPS reading.
-      if(e.provider && e.provider.name == 'gps') stop();
-              
-      setLocation(e);
-      log();      
-    }
-  }    
+  if ( typeof (callback) === 'function') {
+    setLocation(e);
+    log();
+  }
 }
 
 function setLocation(e) {
-  location.latitude = e.coords.latitude;
-  location.longitude = e.coords.longitude;
+  if(e.coords) {
+    location.latitude = e.coords.latitude;
+    location.longitude = e.coords.longitude;
+  }
 }
 
 function log() {
   Ti.API.info('latitude is: ' + location.latitude);
-  Ti.API.info('longitude is: ' + location.longitude);  
+  Ti.API.info('longitude is: ' + location.longitude);
 }
 
 
 function start(obj) {
   callback = obj.action;
   error = obj.error || function(){};
-  
-  timeout = setTimeout(function() { 
-    error(); 
+
+  timeout = setTimeout(function() {
+    error();
     stop();
   }, 25000);
-  
-  Ti.Geolocation.Android.manualMode = true;
-  gpsProvider = Ti.Geolocation.Android.createLocationProvider({
-    name : Ti.Geolocation.PROVIDER_GPS
-  });
-  Ti.Geolocation.Android.addLocationProvider(gpsProvider);
-   
-  Ti.Geolocation.addEventListener("location", firstLocation);    
-    
+
+  Ti.Geolocation.Android.manualMode = false;
+  Titanium.Geolocation.distanceFilter = 1;
+  Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
+  Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
+  Ti.Geolocation.frequency = 1;
+
+  Ti.Geolocation.addEventListener("location", firstLocation);
+
 }
-  
-  function stop() {
-    callback = undefined;
-    error = undefined;
+
+function stop() {
+  callback = undefined;
+  error = undefined;
     if (timeout) clearTimeout(timeout);
-    timeout = undefined;    
-    location = {};
-    
-    Ti.Geolocation.removeEventListener("location", firstLocation);
-    Ti.Geolocation.removeEventListener("location", subsequentLocations);
-  }
-  
-  exports.start = start;
-  exports.stop = stop;
+  timeout = undefined;
+  location = {};
+
+  Ti.Geolocation.removeEventListener("location", firstLocation);
+  Ti.Geolocation.removeEventListener("location", subsequentLocations);
+}
+
+exports.start = start;
+exports.stop = stop;
